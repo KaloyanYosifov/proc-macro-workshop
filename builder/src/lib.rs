a52c2480379770b8910dc9941461a3b51fe86f4a
KaloyanYosifov <kaloqn665@gmail.com>
2021-08-04 08:12:46 +0300

    Generate setters

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -9,14 +9,41 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_struct_name = format!("{}Builder", name);
     let builder_struct_name = syn::Ident::new(&builder_struct_name, name.span());
     let tokens = quote!(
-        struct #builder_struct_name {
+        pub struct #builder_struct_name {
             executable: Option<String>,
             args: Option<Vec<String>>,
             env: Option<Vec<String>>,
             current_dir: Option<String>
         }
+
+        impl #builder_struct_name {
+            pub fn executable(&mut self, exe: String) -> &mut Self {
+                self.executable = Some(exe);
+
+                self
+            }
+
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+
+                self
+            }
+
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+
+                self
+            }
+
+            pub fn current_dir(&mut self, dir: String) -> &mut Self {
+                self.current_dir = Some(dir);
+
+                self
+            }
+        }
+
         impl #name {
-            fn builder() -> #builder_struct_name {
+           pub fn builder() -> #builder_struct_name {
                 #builder_struct_name {
                     env: None,
                     args: None,
