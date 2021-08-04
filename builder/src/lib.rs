d98a87b661d8077573a5d3a0d4e936c77804ba01
KaloyanYosifov <kaloqn665@gmail.com>
2021-08-04 08:07:18 +0300

    Start by integrating test 2 for builder and parsing it

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,21 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let parsed_ast = parse_macro_input!(input as DeriveInput);
+    let name = &parsed_ast.ident;
+    let builder_struct_name = format!("{}Builder", name);
+    let builder_struct_name = syn::Ident::new(&builder_struct_name, name.span());
+    let tokens = quote!(
+        struct #builder_struct_name {}
+        impl #name {
+            fn builder() -> #builder_struct_name {
+                #builder_struct_name {}
+            }
+        }
+    );
 
-    unimplemented!()
+    tokens.into()
 }
